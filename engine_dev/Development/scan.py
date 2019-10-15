7d7497b2bd44092946eed98a60569948497b519c
Reuben Mathew <intelphysic@gmail.com>
2019-10-14 21:53:05 -0400

    notebook scanning is fixed

--- engine_dev/Development/scan.py
+++ engine_dev/Development/scan.py
@@ -1,4 +1,4 @@
-from scanner import Transform 
+import transform 
 from skimage.filters import threshold_local
 import numpy as numpy
 import argparse
@@ -10,59 +10,60 @@ import imutils
 # 	help = 'Path to the image to be scanned')
 # args = vars(ap.parse_args())
 
-class scan:
-	def __init__(self,image):
-		self.image = cv2.imread(image)
+image = cv2.imread('../../images/test2.jpg')
 
-	def process(self):
-		ratio = self.image.shape[0] / 500.0
-		orig = self.image.copy()
-		self.image = imutils.resize(self.image,height = 500)
-		print(self.image.shape)
-		#image processing
-		#Step 1. convert to grayscale
-		#Step 2. blur image (gaussian)
-		#Step 3. utilize Canny edge detection algorithm, uses edge gradients and Sobel kernel
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image,height = 500)
+print(image.shape)
+#image processing
+#Step 1. convert to grayscale
+#Step 2. blur image (gaussian)
+#Step 3. utilize Canny edge detection algorithm, uses edge gradients and Sobel kernel
 
-		gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-		gray = cv2.GaussianBlur(gray, (5,5), 0)
-		edged = cv2.Canny(gray,100,200)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5,5), 0)
+edged = cv2.Canny(gray,100,200)
 
-		# print('Edge Detection...\n')
-		# cv2.imshow('Image',image)
-		# cv2.imshow('Edged',edged)
-		# cv2.waitKey(0)
-		# cv2.destroyAllWindows()
+# print('Edge Detection...\n')
+# cv2.imshow('Image',image)
 
-		cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-		cnts = imutils.grab_contours(cnts)
-		cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
 
-		for c in cnts:
-			peri = cv2.arcLength(c,True)
-			approx = cv2.approxPolyDP(c, 0.01 * peri, True)
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
 
-			if len(approx) == 4:
-				screenCnt = approx
-				break
+cv2.imshow('Edged',edged)
 
-		# print('Contour Detection... ')
-		# cv2.drawContours(image,[screenCnt],-1,(255,0,0), 2)
-		# cv2.imshow('Outline', image)
-		# cv2.waitKey(0)
-		# cv2.destroyAllWindows()
+for c in cnts:
+	peri = cv2.arcLength(c,True)
+	approx = cv2.approxPolyDP(c, 0.01 * peri, True)
 
-		screenCnt = screenCnt.reshape(4,2) * ratio
-		trans = Transform()	
-		warped = trans.four_point_transform(orig,screenCnt)
+	if len(approx) == 4:
+		screenCnt = approx
+		break
+	else:
+		print(len(approx))
 
-		#color and post processing
-		warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-		T = threshold_local(warped, 11, offset = 10, method = 'gaussian')
-		warped = (warped > T).astype('uint8') * 255
+# print('Contour Detection... ')
+cv2.drawContours(image,[screenCnt],-1,(255,0,0), 2)
 
-		return warped
-		# cv2.imshow('Original',imutils.resize(image,height = 650))
-		# cv2.imshow('Transformed',imutils.resize(warped,height = 650))
-		# cv2.waitKey(0)
-		# cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+screenCnt = screenCnt.reshape(4,2) * ratio
+trans = transform.Transform()	
+warped = trans.four_point_transform(orig,screenCnt)
+
+#color and post processing
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+T = threshold_local(warped, 11, offset = 10, method = 'gaussian')
+warped = (warped > T).astype('uint8') * 255
+cv2.imshow('Original',imutils.resize(image,height = 650))
+cv2.imshow('Transformed',imutils.resize(warped,height = 650))
+x = warped.shape[0]
+y = warped.shape[1]
+crop_img = warped[200:y, 100:x]
+cv2.imshow('Cropped',imutils.resize(crop_img))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- engine_dev/Development/transform.py
+++ engine_dev/Development/transform.py
@@ -24,12 +24,12 @@ class Transform:
 		rect = self.order_points(pts)
 		(tl,tr,br,bl) = rect
 
-		widthA = np.sqrt(((br[0] - bl[0])**2) + ((br[1] - bl[1]) ** 2))
-		widthB = np.sqrt(((tr[0] - tl[0])**2) + ((tr[1] - tl[1]) ** 2))
+		widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+		widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
 		maxWidth = max(int(widthA),int(widthB))
 
-		heightA = np.sqrt(((tr[0] - br[0])**2) + ((tr[1] - br[1]) ** 2))
-		heightB = np.sqrt(((tl[0] - bl[0])**2) + ((tl[1] - bl[1]) ** 2))
+		heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+		heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
 		maxHeight = max(int(heightA),int(heightB))
 
 		dst = np.array([
